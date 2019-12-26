f2f6a3a5c965dc9ebc482564a20e9fa4120be3b8
BarbaraL85 <56693064+BarbaraL85@users.noreply.github.com>
2019-12-26 22:41:36 +0100

    Update reverseInParentheses.php

--- reverseInParentheses.php
+++ reverseInParentheses.php
@@ -1,3 +1,6 @@
+#Write a function that reverses characters in (possibly nested) parentheses in the input string.
+#Input strings will always be well-formed with matching ()s.
+
 <?php
 function Reverse1(&$_string)
  {
