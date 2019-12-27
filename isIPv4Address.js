41412d2946aa499764002eb90f76af0eeb16c3fa
BarbaraL85 <56693064+BarbaraL85@users.noreply.github.com>
2019-12-27 23:47:09 +0100

    Update isIPv4Address.js

--- isIPv4Address.js
+++ isIPv4Address.js
@@ -1,1 +1,50 @@
+#An IP address is a numerical label assigned to each device (e.g., computer, printer) participating in a computer network that uses the Internet Protocol for communication. There are two versions of the Internet protocol, and thus two versions of addresses. One of them is the IPv4 address.
+
+#Given a string, find out if it satisfies the IPv4 address naming rules.
+
+#Example
+ #   For inputString = "172.16.254.1", the output should be
+ #   isIPv4Address(inputString) = true;
+
+ #  For inputString = "172.316.254.1", the output should be
+ #  isIPv4Address(inputString) = false.
+
+ #   316 is not in range [0, 255].
+
+ #   For inputString = ".254.255.0", the output should be
+ #   isIPv4Address(inputString) = false.
+
+ #   There is no first number
+ 
+ function isIPv4Address(inputString) {
+       var ifIp=true;
+       if(inputString.includes("."))
+      {
+          var tab = inputString.split(".");
+          var tabLength=tab.length;        
+        if(tabLength!=4) 
+        {
+            ifIp=false;
+        }
+       else
+        {
+            for(i=0;i<tab.length;i++)   
+            {
+              if(tab[i].match(/\D/) == null)
+              {
+              if(tab[i]=="")
+                {
+                  ifIp=false;
+                }
+               if(tab[i]<0||tab[i]>255)
+                {
+                  ifIp=false;
+                }
+              }
+              else{ifIp=false;}
+              }
+        }
+       }else{ifIp=false;}
+       return ifIp;
+}
 
