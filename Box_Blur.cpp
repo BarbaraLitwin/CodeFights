fcab8f3f063317ca8046db8678539b68efd93b09
BarbaraL85 <56693064+BarbaraL85@users.noreply.github.com>
2020-02-07 01:35:31 +0100

    Update Box_Blur.cpp

--- Box_Blur.cpp
+++ Box_Blur.cpp
@@ -1,3 +1,16 @@
+#The algorithm works as follows: each pixel x in the resulting image has a value equal to the average value 
+#of the input image pixels' values from the 3 × 3 square with the center at x. All pixels at the edges are cropped.
+#As pixel's value is an integer, all fractions should be rounded down.
+#Example
+#For
+#image = [[1, 1, 1], 
+#         [1, 7, 1], 
+#         [1, 1, 1]]
+#the output should be boxBlur(image) = [[1]].
+#In the given example all boundary pixels were cropped, and the value of the pixel in the middle was obtained as 
+#(1 + 1 + 1 + 1 + 7 + 1 + 1 + 1 + 1) / 9 = 15 / 9 = 1.66666 = 1. The border pixels are cropped from the final result.
+
+
 std::vector<std::vector<int>> boxBlur(std::vector<std::vector<int>> image) {
 	int lenght=image.size()-2;
 	int width=image[0].size()-2;
